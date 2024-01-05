99eb49a7873c89686d9dcd651c0743f4c1cda885
Marco <marco.boutchelski@gmail.com>
2024-01-05 20:38:46 +0000

    Opacity only on background image

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -3,13 +3,16 @@ import "./HeroSection.css";
 
 function HeroSection() {
    return (
-      <div className="hero-container">
-         {/* <video src="/videos/video-1.mp4" autoPlay loop muted />
-         <h1>ADVENTURE AWAITS</h1>
-         <p>What are you waiting for?</p>
-         <div className="hero-buttons">
-            <button>Get Started</button>
-         </div> */}
+      <div className="container-large">
+         <div className="hero-section text-colour-white">
+            <div>
+               <h1>ADVENTURE AWAITS</h1>
+               <p>What are you waiting for?</p>
+            </div>
+            <div className="hero-buttons">
+               <button>Get Started</button>
+            </div>
+         </div>
       </div>
    );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import Navbar from "../components/Navbar/Navbar";
 import HeroSection from "../components/HeroSection/HeroSection";
 import "./HomePage.css";
 
 export default function HomePage() {
+   const mainRef = useRef(null); // Create a reference to the main element
+
+   useEffect(() => {
+      // After component mounts, calculate and set padding-top
+      const handleResize = () => {
+         if (mainRef.current) {
+            const navbarHeight = document.querySelector("#root > div > nav").offsetHeight;
+            mainRef.current.style.paddingTop = `${navbarHeight}px`;
+         }
+      };
+
+      // Set it initially and also set resize event listener for window
+      handleResize();
+      window.addEventListener("resize", handleResize);
+
+      // Cleanup function to remove the resize listener
+      return () => window.removeEventListener("resize", handleResize);
+   }, []); // Empty dependency array means this effect runs once on mount
+
    return (
-      <div>
-         <Navbar />
-         <HeroSection />
-         <div className="background--img"></div>
-      </div>
+      <>
+         <div className="background-container">
+            <div className="background-img"></div>
+         </div>
+         <div className="content">
+            <Navbar />
+            <main ref={mainRef}>
+               <HeroSection />
+            </main>
+         </div>
+      </>
    );
 }
