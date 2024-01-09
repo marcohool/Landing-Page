8bebaff5be9372de092bc8dc83391186d6a79ff5
Marco Boutchelski <MBK@redwoodtech.com>
2024-01-09 20:22:40 +0000

    Navbar scrolled colour change

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,11 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import Navbar from "../components/Navbar/Navbar";
 import HeroSection from "../components/HeroSection/HeroSection";
 import "./HomePage.css";
 
 export default function HomePage() {
    const mainRef = useRef(null); // Create a reference to the main element
+   const [scrolled, setScrolled] = useState(false);
 
    useEffect(() => {
       // After component mounts, calculate and set padding-top
@@ -23,11 +24,26 @@ export default function HomePage() {
       return () => window.removeEventListener("resize", handleResize);
    }, []); // Empty dependency array means this effect runs once on mount
 
+   useEffect(() => {
+      const handleScroll = () => {
+         const isScrolled = window.scrollY > 0;
+         setScrolled(isScrolled);
+      };
+
+      // Add event listener
+      window.addEventListener("scroll", handleScroll);
+
+      // Clean up function
+      return () => {
+         window.removeEventListener("scroll", handleScroll);
+      };
+   }, []);
+
    return (
       <>
          <div className="content">
             <div className="padding--global">
-               <div className="navbar-fixed-wrapper">
+               <div className={`navbar-fixed-wrapper  ${scrolled ? "scrolled" : ""}`}>
                   <div className="navbar-container">
                      <Navbar />
                   </div>
